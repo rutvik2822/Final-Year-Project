c12ee5b7092fb15304a0e1aa4a1fdde11162d40d
rutvik2822 <102217249+rutvik2822@users.noreply.github.com>
2025-03-07 15:12:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -31,6 +31,10 @@ def predict():
 
     return jsonify({"prediction": "Human" if prediction == 0 else "Bot"})
 
+@app.route('/')
+def home():
+    return "Flask App is Running!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
