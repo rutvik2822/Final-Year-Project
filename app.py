16d7d7862d259d5b51cea0c26c19239ff08ba53d
rutvik2822 <102217249+rutvik2822@users.noreply.github.com>
2025-03-08 14:10:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -33,10 +33,11 @@ def predict():
 
 @app.route('/')
 def home():
-    return "Flask App is Running!"
+    return "Flask is running!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Ensure correct port
+    app.run(host='0.0.0.0', port=port)
 
 
 
