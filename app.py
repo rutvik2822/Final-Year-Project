1676c464f8296112933e5593d2cb69916f232905
rutvik2822 <102217249+rutvik2822@users.noreply.github.com>
2025-03-11 20:59:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from preprocess import preprocess_data
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  
+CORS(app)
 
 # Load trained model
 model = joblib.load("captcha_model.pkl")
